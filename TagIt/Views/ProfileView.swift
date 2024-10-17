e6041a16a52fb229f10005aff41db9961307fce7
Sipeng He <hsp8412@hotmail.com>
2024-10-16 22:15:21 -0600

    Add Profile View and Logout Button

---
+++ TagIt/Views/ProfileView.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileView.swift
+//  TagIt
+//
+//  Created by 何斯鹏 on 2024-10-16.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewModel()
+    var body: some View {
+        Button(action: {
+            viewModel.logout()
+        }) {
+            if viewModel.isLoading {
+                ProgressView()
+            }
+            else{
+                Text("Log Out")
+                    .padding(.horizontal,20)
+                    .padding(.vertical, 15)
+                    .background(.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(25)
+            }
+        }.padding(.top, 20)
+        if let errorMessage = viewModel.errorMessage{
+            Text(errorMessage)
+                .foregroundColor(.red)
+                .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
