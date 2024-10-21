24ded4e94c900f059dab1b938ac5f481be416e28
Peter Tran <peterkhoatran@gmail.com>
2024-10-20 18:32:18 -0600

    Peter: Changed UserService to initialize as a singleton, and changed HomeView to account for this.

--- TagIt/Services/UserService.swift
+++ TagIt/Services/UserService.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 
-class UserService: ObservableObject{
+class UserService: ObservableObject {
+    // init as singleton
+    static let shared = UserService()
+
+    // Private initializer to prevent creating multiple instances
+    private init() {}
+
     @Published var userProfile: UserProfile?
     @Published var errorMessage: String?
     @Published var isLoading: Bool = false
@@ -30,8 +36,7 @@ class UserService: ObservableObject{
     func getUserById(id: String, completion: @escaping (Result<UserProfile, Error>) -> Void) {
         FirestoreService.shared.readDocument(collectionName: FirestoreCollections.user, documentID: id, modelType: UserProfile.self, completion: completion)
     }
-    
-    
 }
+
 
 
