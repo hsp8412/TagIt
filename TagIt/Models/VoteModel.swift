70fa4d8ad04d4a79ce0f7dffd8b02dc69e4ca34d
Peter Tran <peterkhoatran@gmail.com>
2024-10-20 18:58:48 -0600

    Peter: Added ItemType enum to VoteModel and UserCommentModel  to differentiate if a vote is for a comment, deal or view, and if a comment is for a deal for a review. Split initializeAllCollections in FirestoreService into smaller helper functions.

--- TagIt/Models/VoteModel.swift
+++ TagIt/Models/VoteModel.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Peter Tran on 2024-10-20.
 //
+
 import Foundation
 
 struct Vote: Codable {
@@ -11,9 +12,16 @@ struct Vote: Codable {
     let userId: String
     let itemId: String
     let voteType: VoteType
-    
+    let itemType: ItemType
+
     enum VoteType: String, Codable {
         case upvote
         case downvote
+    }
+    
+    enum ItemType: Int, Codable {
+        case comment = 0
+        case deal = 1
+        case review = 2
     }
 }
