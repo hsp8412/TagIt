f10e22bb36a7e73a64290d46d2c5286bb03db059
ChengHouSi <chenghou.si@ucalgary.ca>
2024-11-18 14:57:11 +0800

    Add ranking views. Modify userId to @DocumentID id in UserProfile

---
+++ TagIt/Views/Components/DealRankingView.swift
@@ -0,0 +1,91 @@
+//
+//  DealRankingView.swift
+//  TagIt
+//
+//  Created by Chenghou Si on 2024-11-18.
+//
+
+import SwiftUI
+
+struct DealRankingView: View {
+    @State private var topDeals: [Deal] = []
+    @State private var isLoading: Bool = true
+    @State private var errorMessage: String?
+
+    var body: some View {
+        VStack {
+            // Title
+            HStack {
+                Image(systemName: "trophy.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 30)
+                    .padding(.horizontal, 5)
+                
+                Text("Deal Ranking")
+                    .font(.system(size: 30, weight: .bold))
+            }
+            .overlay(
+                LinearGradient(colors: [.yellow, .orange, .red], startPoint: .top, endPoint: .bottom)
+                    .mask(
+                        HStack {
+                            Image(systemName: "trophy.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 30)
+                                .padding(.horizontal, 5)
+                            
+                            Text("Deal Ranking")
+                                .font(.system(size: 30, weight: .bold))
+                        }
+                    )
+            )
+            .offset(y: 20)
+            
+            // Deals
+            if (isLoading) {
+                ProgressView("Loading deal ranking...")
+                    .frame(maxHeight: .infinity, alignment: .center)
+            } else if let errorMessage = errorMessage {
+                Text("Error: \(errorMessage)")
+            } else {
+                // Ranking cards
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 15) {
+                        ForEach(topDeals.indices, id: \.self) { index in
+                            let deal = topDeals[index]
+                            DealRankingCardView(rank: index+1, deal: deal, fontSize: 25)
+                                .padding(.horizontal)
+                                .padding(.top)
+                        }
+                    }
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
+                .padding()
+                .refreshable {
+                    fetchRanking()
+                }
+            }
+        }
+        .onAppear() {
+            fetchRanking()
+        }
+    }
+    
+    func fetchRanking() {
+        DealService.shared.getDeals { result in
+            switch result {
+            case .success(let fetchedDeals):
+                topDeals = fetchedDeals.sorted { $0.upvote > $1.upvote }
+                self.isLoading = false
+            case .failure(let error):
+                self.errorMessage = error.localizedDescription
+                self.isLoading = false
+            }
+        }
+    }
+}
+
+#Preview {
+    DealRankingView()
+}
--- TagIt/Views/RankingView.swift
+++ TagIt/Views/RankingView.swift
@@ -2,14 +2,73 @@
 //  RankingView.swift
 //  TagIt
 //
-//  Created by 何斯鹏 on 2024-10-07.
+//  Created by Chenghou Si on 2024-11-17.
 //
 
 import SwiftUI
 
 struct RankingView: View {
+    @State private var selected: Int = 0
+
     var body: some View {
-        Text("Ranking")
+        NavigationStack {
+            VStack {
+                HStack {
+                    // User
+                    Button(action: {
+                        withAnimation {
+                            selected = 0
+                        }
+                    }) {
+                        VStack {
+                            Text("Users")
+                                .foregroundColor(selected == 0 ? .blue : .gray)
+                        }
+                        
+                    }
+                    .frame(width: 100)
+                    
+                    // Product
+                    Button(action: {
+                        withAnimation {
+                            selected = 1
+                        }
+                    }) {
+                        VStack {
+                            Text("Products")
+                                .foregroundColor(selected == 1 ? .blue : .gray)
+                        }
+                    }
+                    .frame(width: 100)
+                }
+                .frame(height: 35)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 2)
+                        .frame(height: 3)
+                        .foregroundStyle(.gray.opacity(0.3)),
+                    alignment: .bottom
+                )
+                .overlay(
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 2)
+                            .fill(Color.blue)
+                            .frame(height: 3)
+                            .frame(width: geometry.size.width / 2)
+                            .offset(x: CGFloat(selected) * (geometry.size.width / 2))
+                            .offset(y: 32)
+                            .animation(.easeInOut(duration: 0.3), value: selected)
+                    }
+                )
+                
+                
+                // Conditional rendering
+                if (selected == 0) {
+                    UserRankingView()
+                } else {
+                    DealRankingView()
+                }
+            }
+        }
     }
 }
 
