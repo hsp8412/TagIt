e435930e5a5e0cec80f113278cb49898acca2eef
Mingyang96 <mingyang.li1@ucalgary.ca>
2024-11-19 17:43:58 -0700

    Update BarcodeItemService.swift
    clean up

--- TagIt/Services/BarcodeItemService.swift
+++ TagIt/Services/BarcodeItemService.swift
@@ -8,18 +8,18 @@ class BarcodeItemService {
 
     func getBarcodeItemsByBarcode(barcode: String, completion: @escaping (Result<[BarcodeItemReview], Error>) -> Void) {
         db.collection(FirestoreCollections.revItem)
-            .whereField("barcodeNumber", isEqualTo: barcode) // Query all documents with the matching barcodeNumber
+            .whereField("barcodeNumber", isEqualTo: barcode) 
             .getDocuments { snapshot, error in
                 if let error = error {
-                    completion(.failure(error)) // Return failure if an error occurs
+                    completion(.failure(error)) 
                 } else if let snapshot = snapshot {
                     do {
                         let barcodeItems = try snapshot.documents.map { document in
-                            try document.data(as: BarcodeItemReview.self) // Decode each document into BarcodeItemReview
+                            try document.data(as: BarcodeItemReview.self) 
                         }
-                        completion(.success(barcodeItems)) // Return the list of items on success
+                        completion(.success(barcodeItems)) 
                     } catch {
-                        completion(.failure(error)) // Handle decoding errors
+                        completion(.failure(error)) 
                     }
                 } else {
                     completion(.failure(NSError(domain: "BarcodeItemService", code: -1, userInfo: [NSLocalizedDescriptionKey: "No BarcodeItem found"])))
@@ -29,17 +29,16 @@ class BarcodeItemService {
 
     func addBarcodeItem(barcodeItem: BarcodeItemReview, completion: @escaping (Result<Void, Error>) -> Void) {
         do {
-            // Add to the collection with auto-generated ID
             _ = try db.collection(FirestoreCollections.revItem)
                 .addDocument(from: barcodeItem) { error in
                     if let error = error {
-                        completion(.failure(error)) // Return error if adding fails
+                        completion(.failure(error)) 
                     } else {
-                        completion(.success(())) // Return success
+                        completion(.success(())) 
                     }
                 }
         } catch {
-            completion(.failure(error)) // Handle serialization errors
+            completion(.failure(error))
         }
     }
 
