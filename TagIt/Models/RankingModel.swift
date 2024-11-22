b67f37d08ca0559b78aee5f6f6a7fbe4764fc9ce
Peter Tran <peterkhoatran@gmail.com>
2024-11-21 18:31:46 -0700

    Changed UserProfileModel to have total up/down votes, total deals, total comments and ranking points. Added a ranking model/service class to calculate ranking points. Refactored the rest of the code base to account for these changes.

---
+++ TagIt/Models/RankingModel.swift
@@ -0,0 +1,14 @@
+//
+//  RankingWeights.swift
+//  TagIt
+//
+//  Created by Peter Tran on 2024-11-21.
+//
+
+// Ranking weights model
+private struct RankingWeights {
+    let dealWeight: Int
+    let upvoteWeight: Int
+    let commentWeight: Int
+}
+
